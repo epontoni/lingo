abc2b483a3c3a6c90be5024fee4bff6f7534fb99
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2024-04-13 16:07:14 -0300

    Authentication with Clerk. Hero section of the landing page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -16,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
