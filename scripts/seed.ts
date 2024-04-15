9937763c05b16df720d5e666bed8b64d7d5803d2
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2024-04-14 23:16:17 -0300

    Challenge Cards

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,118 +1,114 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http"
-import { neon } from "@neondatabase/serverless"
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
-import * as schema from "../db/schema"
+import * as schema from "../db/schema";
 
-const sql = neon(process.env.DATABASE_URL!)
-const db = drizzle(sql, { schema})
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql, { schema });
 
 const main = async () => {
-    try {
-        console.log("Seeding database")
-        
-        await db.delete(schema.courses)
-        await db.delete(schema.userProgress)
-        await db.delete(schema.units)
-        await db.delete(schema.lessons)
-        await db.delete(schema.challenges)
-        await db.delete(schema.challengeOptions)
-        await db.delete(schema.challengeProgress)
+  try {
+    console.log("Seeding database");
 
-        await db.insert(schema.courses).values([
-            {
-                id: 1,
-                title: "Spanish",
-                imageSrc: "/es.svg"
-            },
-            {
-                id: 2,
-                title: "Italian",
-                imageSrc: "/it.svg"
-            },
-            {
-                id: 3,
-                title: "French",
-                imageSrc: "/fr.svg"
-            },
-            {
-                id: 4,
-                title: "Croatian",
-                imageSrc: "/hr.svg"
-            },
-        ]);
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
-        await db.insert(schema.units).values([
-            {
-                id:1,
-                courseId: 1,
-                title: "Unit 1",
-                description: "Learn the basics of Spanish",
-                order: 1
-            }
-        ])
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: "Spanish",
+        imageSrc: "/es.svg",
+      },
+      {
+        id: 2,
+        title: "Italian",
+        imageSrc: "/it.svg",
+      },
+      {
+        id: 3,
+        title: "French",
+        imageSrc: "/fr.svg",
+      },
+      {
+        id: 4,
+        title: "Croatian",
+        imageSrc: "/hr.svg",
+      },
+    ]);
 
-        await db.insert(schema.lessons).values([
-            {
-                id:1,
-                unitId: 1,
-                title: "Nouns",
-                order: 1
-            },
-            {
-                id:2,
-                unitId: 1,
-                title: "Verbs",
-                order: 2
-            }
-        ])
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Learn the basics of Spanish",
+        order: 1,
+      },
+    ]);
 
-        await db.insert(schema.challenges).values([
-            {
-                id: 1,
-                lessonId: 1,
-                type: "SELECT",
-                order: 1,
-                question: "Wich one of these is 'The man'?"
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        title: "Nouns",
+        order: 1,
+      },
+      {
+        id: 2,
+        unitId: 1,
+        title: "Verbs",
+        order: 2,
+      },
+    ]);
 
-            }
-        ])
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        order: 1,
+        question: "Wich one of these is 'The man'?",
+      },
+    ]);
 
-        await db.insert(schema.challengeOptions).values([
-            {
-                id: 1,
-                challengeId: 1,
-                imageSrc: "/man.svg",
-                correct: true,
-                text: "el hombre",
-                audioSrc: "/es_man.mp3",
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/robot.svg",
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+      },
+    ]);
 
-            },
-            {
-                id: 2,
-                challengeId: 1,
-                imageSrc: "/woman.svg",
-                correct: false,
-                text: "la mujer",
-                audioSrc: "/es_woman.mp3",
+    console.log("Seed complete");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to seed database");
+  }
+};
 
-            },
-            {
-                id: 3,
-                challengeId: 1,
-                imageSrc: "/robot.svg",
-                correct: false,
-                text: "el robot",
-                audioSrc: "/es_robot.mp3",
-
-            }, 
-        ])
-        
-        console.log("Seed complete")
-    } catch (error) {
-        console.error(error)
-        throw new Error("Failed to seed database")
-    }
-}
-
-main()
+main();
